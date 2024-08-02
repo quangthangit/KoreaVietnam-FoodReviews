bf8bd1d6dfee79b08bb32969e42977ca7b815f23
gemma0911 <thangtq.it@gmial.com>
2024-08-02 20:53:44 +0700

    ADD HEADER

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import './Header.css';
+
 const Header = () => {
-    return (
-        <>
-            Đây là header
-        </>
-    )
+  return (
+    <header>
+      <div className="logo">
+        <img src="/logo.png" alt="Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
